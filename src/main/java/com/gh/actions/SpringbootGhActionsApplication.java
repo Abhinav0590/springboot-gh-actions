72a7b01793d5de4af1c4447487df86482cd4c041
abhinav <abhinavvermabit@gmail.com>
2020-11-28 08:09:56 +0100

    intial commit

---
+++ src/main/java/com/gh/actions/SpringbootGhActionsApplication.java
@@ -0,0 +1,13 @@
+package com.gh.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGhActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGhActionsApplication.class, args);
+	}
+
+}
