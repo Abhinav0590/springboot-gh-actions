3c749d1c90b0a40a69ec017f7fe549bd446b2d9b
abhinav <abhinavvermabit@gmail.com>
2020-11-28 08:24:38 +0100

    adding test controller

---
+++ src/main/java/com/gh/actions/controllers/HelloGhActionsController.java
@@ -0,0 +1,15 @@
+package com.gh.actions.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloGhActionsController {
+
+    @GetMapping("/hello")
+    public String getGreetings() {
+        return "hello from gh-actions controller";
+
+    }
+
+}
